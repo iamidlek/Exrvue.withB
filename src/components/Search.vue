631e471e9c5168ae007720a46718926423552dd1
iamidlek <iamidlek@naver.com>
2021-08-28 23:59:55 +0900

    add Search vue

---
+++ src/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="'search'">
+    this is search page
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.search {
+  position: absolute;
+  top: 2.4rem;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
