06ff17bff3dd89d7e4835f95e7b0d56f3f42194b
iamidlek <iamidlek@naver.com>
2021-08-28 23:58:45 +0900

    create cross vue

---
+++ src/components/Cross.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :class="'cross'">
+    this is cross page
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.cross {
+  position: absolute;
+  top: 2.4rem;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
