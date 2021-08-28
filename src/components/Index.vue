adf714b64d31871bc92a9d9354306660a244b233
iamidlek <iamidlek@naver.com>
2021-08-28 23:59:36 +0900

    add Index vue

---
+++ src/components/Index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Testament</h1>
+    <div :class="'old'">
+      <ol>
+        <li>d</li>
+      </ol>
+    </div>
+    <div :class="'new'">
+      <ol>
+        <li>d</li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+// .index {
+//   position: absolute;
+//   top: 2.4rem;
+//   bottom: 0;
+//   left: 0;
+//   right: 0;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: flex-start;
+//   align-items: center;
+//   padding: 7px;
+//   h1 {
+//     font-size: 20px;
+//   }
+// }
+</style>
