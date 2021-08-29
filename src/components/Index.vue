531d8ab2e8233b6ce5b57ef973da91382b2db4cc
iamidlek <iamidlek@naver.com>
2021-08-29 22:49:06 +0900

    feat: add button and style

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="list">
+      <h1>Testament</h1>
       <div class="select">
-        <h1>Testament</h1>
         <button>Old</button>
         <button>New</button>
       </div>
@@ -27,6 +27,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$index: #1B3C59;
+$search: #A18E7C;
+$cross: #F28177;
+$outcross: #5B7F77;
+$other: #B1B1AC;
+$white-font: #F2E5D5;
+$border-line: #DEB887;
+
 .list {
   position: absolute;
   top: 2.8rem;
@@ -36,23 +44,37 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 7px;
+  padding: 0.4375rem;
+  h1 {
+    line-height: 1;
+    text-align: center;
+    font-size: calc(10vw + 1rem);
+    font-weight: 700;
+    color: $cross;
+    margin: 2rem 0;
+    -webkit-mask-image: 
+    -webkit-gradient(linear,left top,left bottom,
+    color-stop(0,$white-font),
+    color-stop(.5,$white-font),
+    color-stop(1,transparent));
+  }
   .select{
-    h1 {
-      line-height: 1;
-      transition: background-position 1s, background-color 1s;
-      text-align: center;
-      font-size: calc(10vw + 1rem);
-      font-weight: 400;
-      color: #F28177;
-      margin: 1rem 0;
-      -webkit-mask-image: 
-      -webkit-gradient(linear,left top,left bottom,
-      color-stop(0,#FFF),
-      color-stop(.5,#FFF),
-      color-stop(1,transparent));
+    width: 85vw;
+    display: flex;
+    justify-content: center;
+    padding-top: 1rem;
+    button {
+      width: 6.25rem;
+      height: 6.25rem;
+      font-size: 2rem;
+      color: $white-font;
+      background: transparent;
+      border-radius: 40%;
+      border-color: $border-line;
+      &:first-child{
+        margin-right: 5vh;
+      }
     }
-
   }
 }
 </style>
