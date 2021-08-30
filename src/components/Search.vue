1d317291485fb14e3ee21c8ff6b93521ceee696a
iamidlek <iamidlek@naver.com>
2021-08-30 20:46:15 +0900

    feat: add seartch bar in search tab

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,16 @@
 <template>
-  <div :class="'search'">
-    this is search page
+  <div class="search">
+    <form class="search-form">
+      <input
+        type="text"
+        placeholder="gen 1:1"
+        onfocus="this.placeholder = ''"
+        onblur="this.placeholder = 'gen 1:1'" />
+      <ul class="suggestions">
+        <li>F</li>
+        <li>o</li>
+      </ul>
+    </form>
   </div>
 </template>
 
@@ -11,16 +21,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$index: #1B3C59;
+$search: #A18E7C;
+$cross: #F28177;
+$outcross: #5B7F77;
+$other: #B1B1AC;
+$white-font: #F2E5D5;
+$border-line: #DEB887;
+
 .search {
-  position: absolute;
-  top: 2.4rem;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  padding: 7px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: center;
+  position: relative;
+  .search-form{
+    width: 80vw;
+    margin: 5rem auto 0;
+    // overflow: hidden;
+    input {
+      width: 15rem;
+      display: block;
+      margin: 0 auto;
+      font-size: 1.4rem;
+      text-align: center;
+      height: 2.8rem;
+      border: 7px solid #F7F7F7;
+      border-radius: 5px;
+      outline: 0;
+      box-shadow: 
+      0 0 5px rgba(0, 0, 0, 0.12), 
+      inset 0 0 2px rgba(0, 0, 0, 0.19);
+      // z-index: 2;
+    }
+  }
 }
 </style>
