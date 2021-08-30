990071626b5df816879138c3fcd041453137988e
iamidlek <iamidlek@naver.com>
2021-08-31 00:45:22 +0900

    fix: list style

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,8 +6,13 @@
         placeholder="God created heaven"
         onfocus="this.placeholder = ''"
         onblur="this.placeholder = 'God created heaven'" />
-      <div class="scroll">
+      <div class="resultlist">
         <ul class="suggestions">
+          <li>1.In the beginning God created the heaven and the earth.</li>
+          <li>2.And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters.</li>
+          <li>3.And God said, Let there be light: and there was light.</li>
+          <li>4.And God saw the light, that it was good: and God divided the light from the darkness.</li>
+        
           <li>1.In the beginning God created the heaven and the earth.</li>
           <li>2.And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters.</li>
           <li>3.And God said, Let there be light: and there was light.</li>
@@ -54,28 +59,36 @@ $border-input: #F7F7F7;
       0 0 0.4375rem rgba(0, 0, 0, 0.12), 
       inset 0 0 0.125rem rgba(0, 0, 0, 0.19);
     }
-    .scroll {
+    .resultlist {
       position: relative;
       overflow: auto;
-      margin: 4vh 0 2vh;
-      padding: 3vh 0 3vh;
+      height: 35vh;
+      margin: 5.5vh 0 5vh;
       -ms-overflow-style: none; /* IE and Edge */
       scrollbar-width: none; /* Firefox */
       &::-webkit-scrollbar {
-        display: none; /* Chrome, Safari, Opera*/
+      display: none; /* Chrome, Safari, Opera*/
+      }
       .suggestions {
-        margin: 3rem 0 0;
-        padding: 0;
-        position: relative;
-        width: 80vw;
+        display: flex;
+        justify-items: center;
+        flex-direction: column;
         li {
-          width: 70vw;
-          font-size: 1.3rem;
-          line-height: 1.2;
-          font-weight: 400;
           text-align: start;
-          padding: 7px 5px;
-          }
+          width: 100%;
+          font-size: 3vh;
+          line-height: 4vh;
+          color: $white-font;
+        }
+        li:nth-child(even){
+          background: 
+          linear-gradient(to bottom,  
+          $outcross 0%,#EFEFEF 100%);
+        }
+        li:nth-child(odd){
+          background: 
+          linear-gradient(to top,  
+          $outcross 0%,transparent 100%);
         }
       }
     }
