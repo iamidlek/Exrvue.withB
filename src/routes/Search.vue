8d85b4a83be7311e60665d50fa04328e54b76133
iamidlek <iamidlek@naver.com>
2021-09-05 21:24:43 +0900

    apply route

---
+++ src/routes/Search.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>temp</div>
+</template>
+
+<script>
+// import SearchBar from '~/components/SearchBar'
+
+
+// export default {
+//   components:{
+//     SearchBar
+//   },
+// }
+</script>
+
+<style lang="scss">
+@import "~/scss/style";
+</style>
