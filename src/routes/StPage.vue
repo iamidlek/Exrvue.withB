e969a22562ee6d7d56319d8cffa86b1cf08dd9ce
iamidlek <iamidlek@naver.com>
2021-09-05 22:49:39 +0900

    add routerlink

--- src/routes/StPage.vue
+++ src/routes/StPage.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container-fluid">
-    <button
-      type="button"
+    <RouterLink
+      :to="'/index'"
+      active-class="active"
       class="btn btn-primary index">
       INDEX
-    </button>
-    <button
-      type="button"
+    </RouterLink>
+    <RouterLink
+      :to="'/search'"
+      active-class="active"
       class="btn btn-success search">
       SEARCH
-    </button>
+    </RouterLink>
   </div>
 </template>
 
@@ -24,14 +26,16 @@
   height: 98vh;
   padding: 3vh;
   background: linear-gradient(298deg, rgba(55,116,182,1) 0%, rgba(87,9,121,1) 100%);
-  button {
-    display: block;
+  a {
+    display: flex;
     margin-top: 1vh;
     height: 42vh;
     width: 105%;
     border: none;
     font-size: 8vh;
     transition: all .6s;
+    justify-content: center;
+    align-items: center;
     &.index {
       border-bottom-left-radius: 0.8em;
       border-top-left-radius: 0.8em;
