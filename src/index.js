5e9089fff07ecf2e670b1437506dde89ab5ea197
iamidlek <iamidlek@naver.com>
2021-09-09 19:48:22 +0900

    search complete

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import App from './App'
 import '~/scss/style.scss'
 import router from './routes/route.js'
+import store from './store'
 
 createApp(App)
   .use(router)
+  .use(store)
   .mount('#app')
