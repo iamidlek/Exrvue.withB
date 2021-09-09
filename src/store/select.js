9406afed0d5ff9b23ca731cd341a80cec52119a8
iamidlek <iamidlek@naver.com>
2021-09-09 19:49:13 +0900

    fix bible doubled

---
+++ src/store/select.js
@@ -0,0 +1,28 @@
+
+export default {
+  namespaced: true,
+  state: function () {
+    return{
+      selected: [],
+      bible:[]
+    }
+  },
+  getters: {
+
+  },
+  mutations:{
+    loadBible (state) {
+      state.bible = []
+      const endpoint = 'https://gist.githubusercontent.com/iamidlek/08a5d8759ff657352c3cc6418c635590/raw/ea99fa8105dbe8e6759c9aff54b34d5abdff801a/en_en.json';
+      fetch(endpoint)
+      .then(blob => blob.json())
+      .then(data => state.bible.push(...data));
+    },
+    select(state, payload) {
+      state.selected = payload.selected
+    },
+  },
+  actions:{
+    
+  }
+}
